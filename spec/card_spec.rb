f54db2b860c226f0bef92a6fafeadbf94b9eded7
taylorOD <taylorsunwantedemails@gmail.com>
2022-11-28 15:55:25 -0600

    make specs pass again

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,23 @@
 class Card
-  attr_reader :type
+  attr_reader :suit, :rank
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @suit = suit
+    @rank = rank
   end
 end
 
-RSpec.describe 'Card' do
-  it 'has a type' do
-    card = Card.new("Ace of Spades")
-    expect(card.type).to eq('Ace of Spades')
+RSpec.describe Card do
+  it 'has a rank' do
+    card = Card.new("Ace", "Spades")
+
+    expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a suit' do
+    card = Card.new("Ace", "Spades")
+
+    expect(card.rank).to eq('Ace')
   end
 end
 
